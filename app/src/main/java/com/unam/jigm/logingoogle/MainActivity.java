d88e6fd8ead914706793fea8c6ec272b95d18e41
Mario <jigm0406@gmail.com>
2016-09-14 17:09:59 -0500

    login

---
+++ app/src/main/java/com/unam/jigm/logingoogle/MainActivity.java
@@ -0,0 +1,132 @@
+package com.unam.jigm.logingoogle;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.unam.jigm.logingoogle.R.id.sign_in_button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener , GoogleApiClient.OnConnectionFailedListener {
+    //Signin button
+    private SignInButton signInButton;
+
+    //Signing Options
+    private GoogleSignInOptions gso;
+
+    //google api client
+    private GoogleApiClient mGoogleApiClient;
+
+    //Signin constant to check the activity result
+    private int RC_SIGN_IN = 100;
+
+    //TextViews
+    @BindView(R.id.textViewName) TextView textViewEmail;
+    @BindView(R.id.textViewEmail) TextView textViewName;
+    @BindView(R.id.profileImage) SimpleDraweeView profilePhoto;
+    //Image Loader
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Fresco.initialize(this);
+
+        //Initializing Views
+
+        //Initializing google signin option
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        //Initializing signinbutton
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(gso.getScopeArray());
+
+        //Initializing google api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        //Setting onclick listener to signing button
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+//This function will option signing intent
+        private void signIn() {
+            //Creating an intent
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+
+            //Starting intent for result
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //If signin
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //Calling a new function to handle signin
+            handleSignInResult(result);
+        }
+    }
+
+
+    //After the signing we are calling this function
+    private void handleSignInResult(GoogleSignInResult result) {
+        //If the login succeed
+        if (result.isSuccess()) {
+            //Getting google account
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            //Displaying name and email
+            textViewName.setText(acct.getId()+ " " +acct.getDisplayName());
+            textViewEmail.setText(acct.getEmail());
+            Log.d("image:",acct.getPhotoUrl().toString() );
+             Uri uri = Uri.parse(acct.getPhotoUrl().toString());
+
+            profilePhoto.setImageURI(uri);
+
+
+        } else {
+            //If login fails
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
